f9f5112edf7dc8c8041a8e41923e7ef1a3200a1f
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-03-31 18:26:11 +0200

    resolved zad1

--- Zadanie 1/XYZExporter/XYZExporter/Program.cs
+++ Zadanie 1/XYZExporter/XYZExporter/Program.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace XYZExporter
@@ -10,59 +12,103 @@ namespace XYZExporter
     {
         static async Task Main(string[] args)
         {
-
-            string filePath = args[0];
-        //    string destPath = "";
-            string destPath = "C:\\Users\\48501\\OneDrive\\Documents\\APBD\\Jason\\pliki wynikowe";
-           string format = "jason";
-        //   string format = args[2].ToLower();
-
-            //1 opcja
-            // List<string> list = new List<string>();
-            // using (StreamReader stream = new StreamReader(@"C:\Users\48501\OneDrive\Documents\APBD\Jason\dane.csv"))
-            // {
-            //    string line;
-            //    while ((line = stream.ReadLine()) != null)
-            //   {
-            //        list.Add(line);
-            //    }
-            //  }
-            //   using (StreamWriter stream = new StreamWriter("opcja1.txt"))
-            //  {
-            //      foreach (var item in list)
-            //      {
-            //          stream.WriteLine(item);
-            //      }
-            //  }
-            //2 opcja
-            if (format.Equals("jason"))
+            using StreamWriter sw = new StreamWriter("../../../Files/log.txt");
+            if (args.Length != 3)
             {
-                string[] result2 = await File.ReadAllLinesAsync(filePath);
+                throw new Exception("wrong number of arguments");
+            }
 
-                await File.WriteAllLinesAsync(destPath, result2);
+            string pathToDataFile = "";
+            string pathToOutputFile = "";
 
-                string a = "";
-                string[] b = a.Split(',');
-                new Student { };
-                //var jObject = new JObject();
-                // { }
+            if (!File.Exists(Path.GetFullPath(args[0])))
+            {
+                sw.WriteLine($"File not found: {args[0]}");
+            }
+            else
+            {
+                pathToDataFile = Path.GetFullPath(args[0]);
+                Console.WriteLine(pathToDataFile);
+            }
+
+            try
+            {
+                using (File.Create(args[1] + ".jason")) ;
+                pathToOutputFile = Path.GetFullPath(args[1] + ".jason");
+                Console.WriteLine(pathToOutputFile);
+            }
+            catch (Exception e)
+            {
+                sw.WriteLine(e.Message);
+            }
+
+            if (args[2] == "jason")
+            {
+                string[] result2 = await File.ReadAllLinesAsync(pathToDataFile);
+                bool validated = true;
+
                 var studenci = new JArray();
-                // []
-                var jProperty = new JProperty("property", 1);
-                // nazwaProperty: ""
-                //Root: {}, []
-                var jObject = new JObject(
+
+                foreach (string line in result2)
+                {
+                    string alineData = line;
+
+                    string[] studentData = alineData.Split(',');
+
+                    if ((studentData.Length != 9))
+                    {
+                        validated = false;
+                        sw.WriteLine($"Not described by 9 data columns: {alineData}");
+                    }
+                    else
+                    {
+                        foreach (string data in studentData)
+                        {
+                            if (data == "")
+                            {
+                                validated = false;
+                                sw.WriteLine($"One or more columns are empty: {alineData}");
+                            }
+                        }
+                    }
+
+                    if (validated)
+                    {
+                        var newStudent = new JObject(
+                            new JProperty("indexNumber", "s" + studentData[4]),
+                            new JProperty("fname", studentData[0]),
+                            new JProperty("lname", studentData[1]),
+                            new JProperty("birthdate", studentData[5]),
+                            new JProperty("email", studentData[6]),
+                            new JProperty("mothersName", studentData[7]),
+                            new JProperty("fathersName", studentData[8]),
+                            new JProperty("studiesName", studentData[2]),
+                            new JProperty("studiesMode", studentData[3])
+                            );
+
+                        if (studenci.Count == 0)
+                        {
+                            studenci.Add(newStudent);
+                        }
+
+                        if (studenci.Contains(newStudent))
+                        {
+                            sw.WriteLine("Student exist" + newStudent.ToString());
+                        }
+                        else
+                        {
+                            studenci.Add(newStudent);
+                        }
+                    }
+                    var jObject = new JObject(
                     new JProperty("uczelnia", new JObject(
                         new JProperty("createdAt", DateTime.Today.ToString("dd.MM.yyyy")),
-                        new JProperty("author", "Michał Pazio"),
+                        new JProperty("author", "Tomasz Pyrzak"),
                         new JProperty("studenci", studenci)
                     ))
-                );
-                Console.WriteLine(jObject);
-            }
-            else 
-            {
-                Console.WriteLine("Podano nieprawidłowy format danych wyjściowych");
+                    );
+                    File.WriteAllText(pathToOutputFile, jObject.ToString());
+                }
             }
         }
     }
--- Zadanie 1/XYZExporter/XYZExporter/Student.cs
+++ Zadanie 1/XYZExporter/XYZExporter/Student.cs
@@ -1,15 +1,23 @@
-﻿namespace XYZExporter
+﻿using System;
+
+namespace XYZExporter
 {
     internal class Student
     {
-        int indexNumber;
-        string fname;
-        string lname;
-        string birthdate;
-        string email;
-        string mothersName;
-        string fathersName;
-        string studiesName;
-        string studiesmode;
+        public Student(string v1, string v2, string v3, string v4, string v5, string v6, string v7, string v8, string v9)
+        {
+        }
+
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string studiesName { get; set; }
+        public string studiesMode { get; set; }
+        public int indexNumber { get; set; }
+        public DateTime birthdate { get; set; }
+        public string email { get; set; }
+        public string fathersName { get; set; }
+        public string mothersName { get; set; }
     }
+
+
 }
